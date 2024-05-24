abfa199ee258acd54959bbd88694b4db3b2423b7
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-05-25 02:46:26 +0530

    eureka client  config

--- src/main/java/com/lifepill/possystem/PosSystemApplication.java
+++ src/main/java/com/lifepill/possystem/PosSystemApplication.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -33,6 +34,7 @@ import org.springframework.web.client.RestTemplate;
 		)
 )
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients
 @EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl") // Enable JPA Auditing
 public class PosSystemApplication {
